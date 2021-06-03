e400c26bdee7cbb1382c146cdaec3c4f21dbc9d2
zarmby <armando_ahrslade@hotmail.com>
2021-06-03 07:30:17 -0700

    se agregaron los cambios finales del menu page

--- src/Components/menuPage/MenuPage.jsx
+++ src/Components/menuPage/MenuPage.jsx
@@ -1,35 +1,28 @@
 import React, { useState } from 'react';
 import './MenuPageStyles.css'
 
-import inventario from '../../assets/img/inventario_azul.png';
+import inventarioU from '../../assets/img/inventario_azul.png';
+import inventarioS from '../../assets/img/inventario_rojo.png';
 import usuario from '../../assets/img/usuario.png';
 
 function MenuPage() {
 
-  const [img1State,setImg1State] = useState (inventario);
+  const [img1State,setImg1State] = useState (inventarioU);
 
-  const cambiarImagen = () => {
-            inventario.src="../../assets/img/inventario_rojo.png";
-        }
-
-  function restaurarImagen() {
-            inventario.src="../../assets/img/usuario_azul.png";
-        }
 
     return(
       <div>
         <div class="content">
-          <div class="card" onmouseover={cambiarImagen}>
-            <div class="icon"><img class="material-icons md-18" src = {inventario}  onmouseout="restaurarImagen()"></img></div>
+          <div class="card"  onMouseEnter={() => setImg1State(inventarioS)} onMouseLeave={() => setImg1State(inventarioU)}>
+            <div class="icon"><img class="material-icons md-18" src = {img1State}  onmouseout="restaurarImagen()"></img></div>
               <p class="title">Inventario</p>
-              <p class="text">Administra el inventario de equipos disponibles y assi</p>
+              <p class="text">Administra el inventario de equipos disponibles y asi</p>
             </div>
           <div class="card">
-          <div class="icon"><img class="material-icons md-18" src = {usuario}></img></div>
-            <p class="title">Reportes y Notificaciones</p>
-            <p class="text">Descripcion del modulo</p>
-          </div>
-
+            <div class="icon"><img class="material-icons md-18" src = {usuario}></img></div>
+              <p class="title">Reportes y Notificaciones</p>
+              <p class="text">Descripcion del modulo</p>
+            </div>
           </div>
       </div>
     );
