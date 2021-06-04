7c102b259c8f14b373c6f21ee18e24cf933de696
Juan Chavez <chavez.gamez.juan12@gmail.com>
2021-06-04 11:20:13 -0500

    Pantalla de busqueda de usuario y navbar

--- src/Components/user/User.jsx
+++ src/Components/user/User.jsx
@@ -14,7 +14,7 @@ class User extends React.Component{
             <div id="user-container">
                 <Navbar/>
                 <div id="user_page">
-                    <h1>Usuarios</h1>
+                    <h1>Informacion de usuario</h1>
                     <div id="user_search">
                         <input type="text" id="user_search_input" placeholder="Escriba el nombre del usuario a consultar" />
                         <input type="image" src={SearchIcon} id="user_search_btn"/>
