ded4f5003eee24c073ef8cd7bc63de691c2f8ef9
zarmby <armando_ahrslade@hotmail.com>
2021-06-10 08:49:43 -0700

    se agrego el side bar y el listado de computadoras

--- src/Components/inventory/inventoryStock/SingleElement.jsx
+++ src/Components/inventory/inventoryStock/SingleElement.jsx
@@ -5,17 +5,17 @@ function SingleElement(){
 
   return(
     <a href="" class="card">
-      <img src="https://www.officedepot.com.mx/medias/85814.jpg-1200ftw?context=bWFzdGVyfHJvb3R8Mjk3MDc5fGltYWdlL2pwZWd8aDA4L2gyNy85NTk1MjEwNTMwODQ2LmpwZ3wyMThhM2QzZjQ2MjllNTBiM2Y1NDljYzJiNzM4MTEzMTQ1ZTZhZmE1MGNlMzU5ZTZiOTRmZjJiMjE5ZTFmZWVj" class="card__image" alt="" />
+      <img src="https://png.pngtree.com/element_our/20190601/ourlarge/pngtree-cartoon-white-laptop-free-button-image_1372158.jpg" class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
           <img class="card__thumb" src="https://img2.freepng.es/20181127/rcc/kisspng-computer-icons-user-scalable-vector-graphics-login-set-menu-personal-settings-px-svg-png-icon-free-do-5bfdc61ed1a448.2136901815433579828587.jpg" alt="" />
           <div class="card__header-text">
-            <h3 class="card__title">Sin Asignar </h3>
+            <h3 class="card__title">Asignado </h3>
             <span class="card__status">Armando Hernandez Rivera</span>
           </div>
         </div>
-        <p class="card__description">Mirar mas Informacion</p>
+        <p class="card__description">Laptop asus 16 Ram Intel Core I9</p>
       </div>
     </a>
   );
