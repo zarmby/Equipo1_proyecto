a5dcc81922ba87a1dc42e4af4799dd32e845a35a
zarmby <Armando_ahrslade@hotmail.com>
2021-06-22 07:56:56 -0700

    se mejoro el inventory stock

---
+++ src/Components/inventory/inventoryStock/ElementInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ElementInfo.css'
+
+function ElementInfo() {
+  return(
+    <div class="modal-screen">
+      <h1>hola</h1>
+    </div>
+  );
+}
+
+export default ElementInfo;
--- src/Components/inventory/inventoryStock/InventoryStock.jsx
+++ src/Components/inventory/inventoryStock/InventoryStock.jsx
@@ -3,6 +3,7 @@ import './InventoryStock.css'
 import SideFilter from '../sideFilters/SideFilters';
 import Navbar from '../../navbar/Navbar';
 import SingleElement from './SingleElement';
+import ElementInfo from './ElementInfo';
 
 function InventoryStock() {
 
@@ -10,23 +11,19 @@ function InventoryStock() {
     <div class="inv-cont">
       <Navbar />
       <SideFilter />
+      <ElementInfo /  >
       <div clas="Filters">
       </div>
       <div class="cont-list">
         <div class ="grid">
           <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement /><SingleElement />
-          <SingleElement />
-          <SingleElement />
-          <SingleElement />
-          
+            <SingleElement />
+              <SingleElement />
+                <SingleElement />
+                  <SingleElement />
+                    <SingleElement />
+                      <SingleElement />
+                        <SingleElement />
         </div>
       </div>
     </div>
