e4f88d11293032883915bdf13bb71f0e90b6efd4
zarmby <Armando_ahrslade@hotmail.com>
2021-06-28 07:24:05 -0700

    .

--- src/Components/inventory/inventoryStock/SingleElement.jsx
+++ src/Components/inventory/inventoryStock/SingleElement.jsx
@@ -11,16 +11,6 @@ function SingleElement(props){
           <svg class="card__arc"></svg>
           <img class="card__thumb" src="https://img2.freepng.es/20181127/rcc/kisspng-computer-icons-user-scalable-vector-graphics-login-set-menu-personal-settings-px-svg-png-icon-free-do-5bfdc61ed1a448.2136901815433579828587.jpg" alt="" />
           <div class="card__header-text">
-<<<<<<< HEAD
-            <h3 class="card__title">Disponible</h3>
-            <img class="status" src="https://emojitool.com/img/facebook/4.0/large-green-circle-4327.png" alt="" />
-            <span class="card__status">N/A</span>
-          </div>
-        </div>
-        <div class="card__description">
-          <p>NS: 34452618 / Marca: Lenovo</p>
-          <p>Sede: AGS / Modelo: XS-mamalouski 2.0</p>
-=======
             <h3 class="card__title">{props.status}</h3>
             <img class="status" src="https://images.emojiterra.com/google/android-10/512px/1f534.png" alt="" />
             <span class="card__status">Armando Hernandez Rivera</span>
@@ -30,7 +20,6 @@ function SingleElement(props){
           <p><strong>N/S /</strong> {props.serialnumber}</p>
           <p><strong>Model /</strong> {props.model}</p>
           <p><strong>Marca /</strong> {props.mark}</p>
->>>>>>> dev
         </div>
       </div>
     </div>
