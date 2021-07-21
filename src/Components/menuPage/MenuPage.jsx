3bb36e8ce39498d0342489305c8bcab2b984c13d
zarmby <Armando_ahrslade@hotmail.com>
2021-07-21 10:17:05 -0700

    .
    .

--- src/Components/menuPage/MenuPage.jsx
+++ src/Components/menuPage/MenuPage.jsx
@@ -7,7 +7,7 @@ import usuarioA from '../../assets/img/usuario_azul.png';
 import usuarioR from '../../assets/img/usuario_rojo.png';
 import Navbar from '../navbar/Navbar';
 import { useRef, useEffect } from 'react';
-import Loading from '../loading/Loading'; 
+import Loading from '../loading/Loading';
 
 function MenuPage() {
   const [loading,setLoading] = useState (true);
@@ -27,14 +27,14 @@ function MenuPage() {
     <div><Navbar ref={child_navbar} />
       {(loading) ? <Loading />
       : <div className="content-option" onClick={child_navbar.current.closeSideMenuNabvar}>
-        <Link to="InventoryCenter" style={{ textDecoration: 'none' }}>
+        <Link id="InventoryOption" to="InventoryCenter" style={{ textDecoration: 'none' }}>
           <div className="option"  onMouseEnter={() => setImg1State(inventarioS)} onMouseLeave={() => setImg1State(inventarioU)}>
             <div className="icon "><img className="material-icons md-18" src = {img1State} alt="Inventario"></img></div>
               <p className="title">Inventario</p>
               <p className="text">Administra el inventario de equipos y mira la informacion disponible sobre ellos.</p>
             </div>
         </Link>
-        <Link to="/user" className="link">
+        <Link id="UserOption" to="/user" className="link">
           <div className="option"  onMouseEnter={() => setImg2State(usuarioR)} onMouseLeave={() => setImg2State(usuarioA)}>
             <div className="icon"><img className="material-icons md-18" src = {img2State} alt="Usuario"></img></div>
               <p className="title">Reportes y Notificaciones</p>
